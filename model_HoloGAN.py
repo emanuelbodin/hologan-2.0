fixed normalization of images so colors are correct

model_HoloGAN.py
@@ -25,8 +25,8 @@ MODELDIR = os.path.join(OUTPUT_DIR, 'models')
 IMG_DIR = os.path.join(OUTPUT_DIR, 'images')
 SAMPLE_DIR = os.path.join(OUTPUT_DIR, "samples")
 
-STEP = 10
-GEN_DIR = "../generated_data/shapes3d"
+STEP = 4
+GEN_DIR = "../generated_data/celeba"
 # ----------------------------------------------------------------------------
 def stop():
   raise Exception('STOPPED')
@@ -77,7 +77,6 @@ class HoloGAN(object):
         self.inputs = tf.compat.v1.placeholder(tf.float32, [
                                      None, self.output_height, self.output_width, self.c_dim], name='real_images')
         self.z = tf.compat.v1.placeholder(tf.float32, [None, cfg['z_dim']], name='z')
-        #self.z_map = tf.Variable(tf.ones([1, cfg['z_dim']]))
         inputs = self.inputs
 
         gen_func = eval("self." + (cfg['generator']))
@@ -126,7 +125,7 @@ class HoloGAN(object):
         #self.G_z_map = gen_func(self.z_map, self.view_in, reuse=True)
 
         if str.lower(str(cfg["style_disc"])) == "true":
-            print("Style disc")
+            print("Style disc") 
             self.d_loss = self.d_loss + self.d_h1_loss + \
                 self.d_h2_loss + self.d_h3_loss + self.d_h4_loss
         # ====================================================================================================================
@@ -153,42 +152,43 @@ class HoloGAN(object):
 
     # =======================================================================================================================
     def generate_images(self, config):
-        sample_z = self.sampling_Z(cfg['z_dim'], str(cfg['sample_z']))
-        images = []
-        could_load, checkpoint_counter = self.load()
-        if could_load:
-            counter = checkpoint_counter
-            print(" [*] Load SUCCESS")
-        else:
-            print(" [!] Load failed...")
-            return
-        if not os.path.exists(self.sample_dir):
-            os.makedirs(self.sample_dir)
-       
-        sample_view = self.gen_view_func(cfg['batch_size'],
-                        cfg['ele_low'], cfg['ele_high'],
-                        cfg['azi_low'], cfg['azi_high'],
-                        cfg['scale_low'], cfg['scale_high'],
-                        cfg['x_low'], cfg['x_high'],
-                        cfg['y_low'], cfg['y_high'],
-                        cfg['z_low'], cfg['z_high'],
-                        with_translation=to_bool(str(cfg['with_translation'])),
-                        with_scale=to_bool(str(cfg['with_scale'])))
+        count = 0
+        for i in range(500):
+          sample_z = self.sampling_Z(cfg['z_dim'], str(cfg['sample_z']))
+          images = []
+          could_load, checkpoint_counter = self.load()
+          if could_load:
+              counter = checkpoint_counter
+              print(" [*] Load SUCCESS")
+          else:
+              print(" [!] Load failed...")
+              return
+          if not os.path.exists(self.sample_dir):
+              os.makedirs(self.sample_dir)
+        
+          sample_view = self.gen_view_func(cfg['batch_size'],
+                          cfg['ele_low'], cfg['ele_high'],
+                          cfg['azi_low'], cfg['azi_high'],
+                          cfg['scale_low'], cfg['scale_high'],
+                          cfg['x_low'], cfg['x_high'],
+                          cfg['y_low'], cfg['y_high'],
+                          cfg['z_low'], cfg['z_high'],
+                          with_translation=to_bool(str(cfg['with_translation'])),
+                          with_scale=to_bool(str(cfg['with_scale'])))
 
-        feed_eval = {self.z: sample_z,
-                         self.view_in: sample_view}
+          feed_eval = {self.z: sample_z,
+                          self.view_in: sample_view}
 
-        samples = self.sess.run(self.G, feed_dict=feed_eval)
-        ren_img1 = inverse_transform(samples)
-        ren_img = np.clip(255 * ren_img1, 0, 255).astype(np.uint8)
-        images.append(ren_img1[0])
-        counter = 0
-        for img in ren_img:
-          img = Image.fromarray(img, 'RGB')
-          img.paste(img)
-          img.save(os.path.join(GEN_DIR, "{0}.jpg".format(counter)),"JPEG")
-          counter = counter + 1
-          print('Image saved: ', counter)
+          samples = self.sess.run(self.G, feed_dict=feed_eval)
+          ren_img1 = inverse_transform(samples)
+          ren_img = np.clip(255 * ren_img1, 0, 255).astype(np.uint8)
+          images.append(ren_img1[0])
+          for img in ren_img:
+            img = Image.fromarray(img, 'RGB')
+            img.paste(img)
+            img.save(os.path.join(GEN_DIR, "{0}.jpg".format(count)),"JPEG")
+            count = count + 1
+            print('Image saved: ', count)
 # =======================================================================================================================
    
 
@@ -233,7 +233,7 @@ class HoloGAN(object):
             print(" [!] Load failed...")
             return
 
-        num_optimization_steps = 200
+        num_optimization_steps = 1000
         losses = []
         print('START')
         feed = { self.view_in: sample_view, self.z: sample_z, self.inputs: sample_img}
@@ -268,6 +268,10 @@ class HoloGAN(object):
         plt.plot(losses)
         plt.xlabel('steps')
         plt.ylabel('loss')
+        plt.ylim(ymin=0)  
+        plt.xlim(xmin=0)  
+        plt.title("Loss per iteration for shapes3d sample")
+        plt.show()
 
         if str.lower(str(cfg["sample_from_z"])) == "true":
           self.sample_from_z(config, sample_z)
@@ -339,13 +343,14 @@ class HoloGAN(object):
                                           resize_height=self.output_height,
                                           resize_width=self.output_width,
                                           crop=self.crop) for batch_file in batch_files]
+                
                 """
-                img = batch_images[0]
-                img = np.clip(255 * batch_images[0] + 127.5, 0, 255).astype(np.uint8)
-                img = Image.fromarray(img, 'RGB')
-                img.show()
+                img = np.clip(255 * batch_images[0], 0, 255).astype(np.uint8)
+                cv2.imshow('image',img)
+                cv2.waitKey(0)
                 raise Exception('å')
                 """
+
                 batch_z = self.sampling_Z(cfg['z_dim'], str(cfg['sample_z']))
                 batch_view = self.gen_view_func(cfg['batch_size'],
                                                 cfg['ele_low'], cfg['ele_high'],
@@ -461,16 +466,22 @@ class HoloGAN(object):
                          self.view_in: sample_view}
 
             samples = self.sess.run(self.G, feed_dict=feed_eval)
-            ren_img1 = inverse_transform(samples)
-            ren_img = np.clip(255 * ren_img1, 0, 255).astype(np.uint8)
-            images.append(ren_img1[0])
+            ren_img = inverse_transform(samples)
+            ren_img = np.clip(255 * ren_img , 0, 255).astype(np.uint8)
+
+            """
+            cv2.imshow('image',ren_img[0])
+            cv2.waitKey(0)
+            raise Exception('å')
+            """
+            
             try:
-                imageio.imwrite(
+                cv2.imwrite(
                     os.path.join(
                         self.sample_dir, "{0}_samples_{1}.jpg".format(counter, i)),
                     merge(ren_img, [cfg['batch_size'] // 4, 4]))
             except:
-                imageio.imwrite(
+                cv2.imwrite(
                     os.path.join(
                         self.sample_dir, "{0}_samples_{1}.jpg".format(counter, i)),
                     ren_img[0])

tools/utils.py
@@ -20,7 +20,8 @@ import tensorflow.contrib.slim as slim
 import glob
 import os
 import random
-import scipy.misc
+
+import matplotlib.pyplot as plt
 
 pp = pprint.PrettyPrinter()
 
@@ -37,6 +38,11 @@ def get_image(image_path, input_height, input_width,
               resize_height=64, resize_width=64,
               crop=True):
     image = load_webp(image_path)
+    """
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    raise Exception('å')
+    """
     crop = False
     if (input_height != resize_height):
       image = cv2.resize(image,(int(resize_height),int(resize_width)))
@@ -94,7 +100,8 @@ def transform(image, input_height, input_width,
     # In case of binary mask with no channels:
     if len(cropped_image.shape) != 3:
         cropped_image = np.expand_dims(cropped_image, -1)
-    return np.array(cropped_image)[:, :, :3]/127.5 - 1.
+    
+    return np.array(cropped_image)[:, :, :3]/255.
 
 
 def inverse_transform(images):